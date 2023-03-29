5d9803ce31d1adf3068569dc7d65a58865778d33
frankenState <ubayfranklou@gmail.com>
2023-03-29 10:05:21 +0800

    fixed the height and width.

--- src/views/MapDemo.vue
+++ src/views/MapDemo.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>Using Leaflet and Open Street Map</h1>
-    <div style="height:600px; width:800px">
+    <div class="h-100 w-100">
         <l-map ref="map" :zoom="40" :center="[7.447130, 125.809503]">
             <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
                 name="OpenStreetMap"></l-tile-layer>
--- src/views/MapDemoTwo.vue
+++ src/views/MapDemoTwo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="height:600px; width:800px">
+    <div style="width: 500px; height: 200px;">
         <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
             <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
                 name="OpenStreetMap"></l-tile-layer>
